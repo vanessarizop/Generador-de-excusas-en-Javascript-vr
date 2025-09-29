2811f354bfc9981802a75094efb6d00a146df85b
vanessarizop <vanessa.rizop@gmail.com>
2025-09-29 16:00:56 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/excusa.png";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+ 
+function excuse(who,action,what,when){
+  let quien=Math.floor(Math.random()*who.length);
+  let accion=Math.floor(Math.random()*action.length);
+  let que=Math.floor(Math.random()*what.length);
+  let cuando=Math.floor(Math.random()*when.length); 
+  return who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando] + ".";
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    document.querySelector('#excuse').innerHTML = excuse(who, action, what, when);
 };
